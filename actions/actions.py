6f6b493ea6a8f95edff9622919f9d529657507a6
naseer444 <66530855+naseer444@users.noreply.github.com>
2022-05-09 18:31:26 +0500

    Locations retrieved till mountains

--- actions/actions.py
+++ actions/actions.py
@@ -11,17 +11,117 @@ from typing import Any, Text, Dict, List
 
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
+import requests
+from rasa_sdk.events import SlotSet, FollowupAction
+
+
+from firebaseRetrieval import funcLake
+from firebaseRetrieval import funcValley
+from firebaseRetrieval import funcNationalParks
+from firebaseRetrieval import funcMosques
+from firebaseRetrieval import funcHill
+from firebaseRetrieval import funcWaterfall
+from firebaseRetrieval import funcMountain
+
 
 
 class ActionHelloWorld(Action):
 
     def name(self) -> Text:
         return "action_hello_world"
-
+    
     def run(self, dispatcher: CollectingDispatcher,
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        
+        
+        dispatcher.utter_message("Here are the Lakes I found in Pakistan:\n\n\n")
+        dispatcher.utter_message(funcLake())
 
-        dispatcher.utter_message(text="Hello World!")
 
-        return []
+class ActionValley(Action):
+    
+    def name(self) -> Text:
+        return "action_valley"
+    
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        
+        dispatcher.utter_message("Here are the Valleys I found in Pakistan:\n\n\n")
+        dispatcher.utter_message(funcValley())   
+        
+        
+class ActionNationalParks(Action):
+    
+    def name(self) -> Text:
+        return "action_NationalParks"
+    
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        
+        dispatcher.utter_message("Here are the National Parks of Pakistan:\n\n\n")
+        dispatcher.utter_message(funcNationalParks()) 
+
+
+class ActionMosques(Action):
+    
+    def name(self) -> Text:
+        return "action_Mosques"
+    
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        
+        dispatcher.utter_message("Here are the Mosques of Pakistan:\n\n\n")
+        dispatcher.utter_message(funcMosques()) 
+        
+        
+        
+
+class ActionHill(Action):
+    
+    def name(self) -> Text:
+        return "action_hill"
+    
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        
+        dispatcher.utter_message("Here are the Hill Stations of Pakistan:\n\n\n")
+        dispatcher.utter_message(funcHill()) 
+        
+
+class ActionWaterfall(Action):
+    
+    def name(self) -> Text:
+        return "action_waterfall"
+    
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        
+        dispatcher.utter_message("Here are the Waterfalls of Pakistan:\n\n\n")
+        dispatcher.utter_message(funcWaterfall()) 
+        
+        
+        
+class ActionMountain(Action):
+    
+    def name(self) -> Text:
+        return "action_mountain"
+    
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        
+        dispatcher.utter_message("Here are some Mountains of Pakistan:\n\n\n")
+        dispatcher.utter_message(funcMountain()) 
+        
+        
+        
+        
+        
+        
+        
---
+++ actions/firebaseRetrieval.py
@@ -0,0 +1,168 @@
+import pyrebase
+
+
+firebaseConfig = {"apiKey": "AIzaSyAeQtvt8zN0yLikTwVcxUhO5rx2wvduOGY",
+                        "authDomain": "planitpk-cf006-restore.firebaseapp.com",
+                        "databaseURL": "https://planitpk-cf006-restore-default-rtdb.firebaseio.com",
+                        "projectId": "planitpk-cf006-restore",
+                        "storageBucket": "planitpk-cf006-restore.appspot.com",
+                        "messagingSenderId": "999842759391",
+                        "appId": "1:999842759391:web:1df0a55e04e431db425f6a",
+                        "measurementId": "${config.measurementId}"
+
+                        }
+
+firebase=pyrebase.initialize_app(firebaseConfig)
+
+def noquote(s):
+    return s
+pyrebase.pyrebase.quote = noquote
+
+
+db=firebase.database()
+
+
+
+def funcLake():
+
+    
+
+    #locations = db.child("Locations").order_by_child("category").get()
+    
+    
+    locations = db.child("Locations").order_by_child("category").equal_to("Lake").get();
+
+    tempArr=[]
+    for loc in locations.each():
+        tempArr.append(loc.val()['name'])
+        tempArr.append(loc.val()['Desc'])
+       
+       
+    values = '\n'.join([str(i) for i in tempArr])
+    
+    return values
+
+
+
+def funcValley():
+    
+    
+
+    #locations = db.child("Locations").order_by_child("category").get()
+    
+    
+    locations = db.child("Locations").order_by_child("category").equal_to("Valley").get();
+
+    tempArr=[]
+    for loc in locations.each():
+        tempArr.append(loc.val()['name'])
+        tempArr.append(loc.val()['Desc'])
+       
+       
+    values = '\n'.join([str(i) for i in tempArr])
+    
+    return values
+        
+
+
+def funcNationalParks():
+    
+    
+
+    #locations = db.child("Locations").order_by_child("category").get()
+    
+    
+    locations = db.child("Locations").order_by_child("category").equal_to("National Park").get();
+
+    tempArr=[]
+    for loc in locations.each():
+        tempArr.append(loc.val()['name'])
+        tempArr.append(loc.val()['Desc'])
+       
+       
+    values = '\n'.join([str(i) for i in tempArr])
+    
+    return values
+
+
+def funcMosques():
+    
+    
+
+    #locations = db.child("Locations").order_by_child("category").get()
+    
+    
+    locations = db.child("Locations").order_by_child("category").equal_to("Mosque").get();
+
+    tempArr=[]
+    for loc in locations.each():
+        tempArr.append(loc.val()['name'])
+        tempArr.append(loc.val()['Desc'])
+       
+       
+    values = '\n'.join([str(i) for i in tempArr])
+    
+    return values
+
+def funcHill():
+    
+    
+
+    #locations = db.child("Locations").order_by_child("category").get()
+    
+    
+    locations = db.child("Locations").order_by_child("category").equal_to("Hill Station").get();
+
+    tempArr=[]
+    for loc in locations.each():
+        tempArr.append(loc.val()['name'])
+        tempArr.append(loc.val()['Desc'])
+       
+       
+    values = '\n'.join([str(i) for i in tempArr])
+    
+    return values
+
+
+def funcWaterfall():
+    
+    
+
+    #locations = db.child("Locations").order_by_child("category").get()
+    
+    
+    locations = db.child("Locations").order_by_child("category").equal_to("Waterfall").get();
+
+    tempArr=[]
+    for loc in locations.each():
+        tempArr.append(loc.val()['name'])
+        tempArr.append(loc.val()['Desc'])
+       
+       
+    values = '\n'.join([str(i) for i in tempArr])
+    
+    return values
+
+
+
+def funcMountain():
+    
+    
+
+    #locations = db.child("Locations").order_by_child("category").get()
+    
+    
+    locations = db.child("Locations").order_by_child("category").equal_to("Mountainous").get();
+
+    tempArr=[]
+    for loc in locations.each():
+        tempArr.append(loc.val()['name'])
+        tempArr.append(loc.val()['Desc'])
+       
+       
+    values = '\n'.join([str(i) for i in tempArr])
+    
+    return values
+
+
+
